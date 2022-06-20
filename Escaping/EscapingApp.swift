0598e923d2e9ad86cd9b3ca22345ab926532a58d
Prathamesh Araikar <prathamesharaikar11@gmail.com>
2022-06-20 14:46:35 +0530

    Initial Commit

---
+++ Escaping/EscapingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EscapingApp.swift
+//  Escaping
+//
+//  Created by Prathamesh Araikar on 20/06/22.
+//
+
+import SwiftUI
+
+@main
+struct EscapingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
